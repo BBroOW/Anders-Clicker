faf71af3f1c23bbbf2dcd2161e2e0526f475c950
BBroOW <brage_hagelund@outlook.com>
2023-11-04 13:45:26 +0100

    lagt til mr anderson coins

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,14 @@ import anders7 from "./assets/images/anders7.png";
 import anders8 from "./assets/images/anders8.png";
 import anders9 from "./assets/images/anders9.png";
 import anders10 from "./assets/images/anders10.png";
+import andersonCoin from "./assets/images/anderson-coin.png";
 import video from "./assets/video/fnaf.mp4";
 import Letsgo from "./assets/music/letsgo.mp3";
 import Nekozilla from "./assets/music/nekozilla.mp3";
 import OnAndON from "./assets/music/onandon.mp3";
 import WhyWeLose from "./assets/music/whywelose.mp3";
+
+const AndersonCoin = andersonCoin;
 
 let image = anders1;
 let randomSong;
@@ -24,11 +27,8 @@ let isSongPlaying = false;
 let audioVolume = 0.2;
 
 function randomNumberMusic() {
-
   const randomNumberSong = Math.floor(Math.random() * 4);
   console.log(randomNumberSong);
-
-  //make it so if the song has been played it can not be played twice in a row
 
   if (randomNumberSong === 0 && randomSong !== Letsgo) {
     randomSong = Letsgo;
@@ -42,43 +42,30 @@ function randomNumberMusic() {
 }
 
 function volumeSlider() {
-  // Update the current slider value (each time you drag the slider handle)
   var slider = document.getElementById("myRange");
   var output = document.getElementById("demo");
-  output.innerHTML = slider.value; // Display the default slider value
+  output.innerHTML = slider.value;
 }
 
 function playMusic() {
-  // Check if a song is currently playing
   if (isSongPlaying) {
     return;
   }
 
-  randomNumberMusic(); // Select a new random song
+  randomNumberMusic();
   song = new Audio(randomSong);
 
-  // set default volume
   song.volume = audioVolume;
-  // Set isSongPlaying to true to indicate that a song is playing
   isSongPlaying = true;
 
-  // Add an event listener for the "ended" event
   song.addEventListener("ended", function () {
-    // Reset isSongPlaying to false when the song ends
     isSongPlaying = false;
-
-    // Call randomNumberMusic again and play the new song
     volumeSlider();
     playMusic();
   });
 
-
-
-  // Play the song
   song.play();
 }
-
-
 
 function handleVolumeChange(event) {
   const newVolume = event.target.value / 100;
@@ -88,41 +75,44 @@ function handleVolumeChange(event) {
     song.volume = newVolume;
   }
 
-  // Update the displayed volume level
   document.getElementById("demo").innerHTML = Math.round(newVolume * 100);
 }
 
-
-
+function AndersonCoinNumberChecker(clicks, setAndersonCoinNumber) {
+  if (clicks >= 10 && clicks % 10 === 0) {
+    const newAndersonCoinNumber = clicks / 10;
+    setAndersonCoinNumber(newAndersonCoinNumber);
+  }
+}
 
 function App() {
-  // State to keep track of trash count
   const [clicks, setClicks] = useState(() => {
-    // Initialize with the value from local storage or default to 0
     return parseInt(localStorage.getItem("clicks")) || 0;
-    volumeSlider() 
-    
   });
 
-  // Function to handle button click
+  const [andersonCoinNumber, setAndersonCoinNumber] = useState(() => {
+    return parseFloat(localStorage.getItem("coins")) || clicks / 10;
+  });
+
+  useEffect(() => {
+    document.getElementById("displayClick").innerHTML = `${clicks} clicks`;
+    AndersonCoinNumberChecker(clicks, setAndersonCoinNumber);
+  }, [clicks, andersonCoinNumber]);
+
   function onClick() {
     playMusic();
-    //scale only the img every time you click and after you click it goes back to normal
+
     document.getElementById("img").style.transform = "scale(1.1)";
     setTimeout(() => {
       document.getElementById("img").style.transform = "scale(1)";
     }, 100);
 
-
-    
     const randomNumber = Math.floor(Math.random() * 1000);
-    console.log(randomNumber);
 
     if (randomNumber === 56) {
       let videoFnaf = document.getElementById("fnaf");
       videoFnaf.style.display = "block";
 
-      // Play video in fullscreen
       if (videoFnaf.requestFullscreen) {
         videoFnaf.requestFullscreen();
       } else if (videoFnaf.mozRequestFullScreen) {
@@ -136,7 +126,6 @@ function App() {
       videoFnaf.play();
 
       setTimeout(() => {
-        // Exit fullscreen
         if (document.exitFullscreen) {
           document.exitFullscreen();
         } else if (document.mozCancelFullScreen) {
@@ -156,83 +145,22 @@ function App() {
     const newClicks = clicks + 1;
     setClicks(newClicks);
 
-    // Store the updated count in local storage
+    AndersonCoinNumberChecker(newClicks, setAndersonCoinNumber);
+
+    localStorage.setItem("coins", andersonCoinNumber.toString());
     localStorage.setItem("clicks", newClicks.toString());
   }
 
-  // Use useEffect to update the DOM when the component mounts
-  useEffect(() => {
-    document.getElementById("displayClick").innerHTML = `${clicks} clicks`;
-  }, [clicks]);
-  
-
-  if (clicks >= 1000) {
-    image = anders2;
-  }
-  if (clicks >= 10000) {
-    image = anders3;
-  }
-  if (clicks >= 100000) {
-    image = anders4;
-  }
-  if (clicks >= 1000000) {
-    image = anders5;
-  }
-  if (clicks >= 10000000) {
-    image = anders6;
-  }
-  if (clicks >= 100000000) {
-    image = anders7;
-  }
-  if (clicks >= 1000000000) {
-    image = anders8;
-  }
-  if (clicks >= 10000000000) {
-    image = anders9;
-  }
-  if (clicks >= 100000000000) {
-    image = anders10;
-  }
-
-/* test images
-  if (clicks >= 10) {
-    image = anders2;
-  }
-  if (clicks >= 20) {
-    image = anders3;
-  }
-  if (clicks >= 30) {
-    image = anders4;
-  }
-  if (clicks >= 40) {
-    image = anders5;
-  }
-  if (clicks >= 50) {
-    image = anders6;
-  }
-  if (clicks >= 60) {
-    image = anders7;
-  }
-  if (clicks >= 70) {
-    image = anders8;
-  }
-  if (clicks >= 80) {
-    image = anders9;
-  }
-  if (clicks >= 90) {
-    image = anders10;
-  }
-*/
   return (
     <div id="page">
-      
       <div id="video">
         <video id="fnaf" src={video} style={{ display: "none" }} />
       </div>
-      {/* Volume slider */}
       <div id="navbar">
         <div id="volume">
-          <p>Volume: <span id="demo"></span></p>
+          <p>
+            Volume: <span id="demo"></span>
+          </p>
           <input
             type="range"
             min="0"
@@ -243,19 +171,23 @@ function App() {
             onChange={handleVolumeChange}
           />
         </div>
-        < h1 id="title">CLICK ANDERS</h1>
+        <h1 id="title">CLICK ANDERS</h1>
         <div id="login">
           <p>login</p>
         </div>
       </div>
-      {/* Main button */}
-      <div id="main-button">
-      <p id="displayClick"></p>
-      <button type="button" onClick={onClick}>
-        <img src={image} id="img" alt="Anders" />
-      </button>
+      <div id="main-page">
+        <div id="coins">
+          <img src={AndersonCoin} alt="coins" />
+          <p id="displayCoin">{andersonCoinNumber}</p>
+        </div>
+        <div id="main-button">
+          <p id="displayClick"></p>
+          <button type="button" onClick={onClick}>
+            <img src={image} id="img" alt="Anders" />
+          </button>
+        </div>
       </div>
-      
     </div>
   );
 }
